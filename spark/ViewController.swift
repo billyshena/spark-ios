e3a3a0f1a53ab30e47c6235e34cd57100c9f62b7
Billy Shen <billy.shen@hetic.net>
2015-10-06 19:42:46 +0200

    [ADD] iOS spark initial project

--- spark/ViewController.swift
+++ spark/ViewController.swift
@@ -10,9 +10,27 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var progressView: UIView!
+    @IBOutlet weak var deckView: UIView!
+    @IBOutlet weak var segmentedControl: UISegmentedControl!
+    @IBOutlet weak var completedView: UIView!
+    
     override func viewDidLoad() {
+        
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        
+        // Show the first view and hide the other ones
+        deckView.hidden = false
+        completedView.hidden = true
+        progressView.hidden = true
+        
+        // Remove border on navigation bar
+        self.navigationController?.navigationBar.clipsToBounds = true
+        
+        // Create logo UIImage object and set it as the navigation main title
+        let image = UIImage(named: "logo.png")
+        self.navigationItem.titleView = UIImageView(image: image)
+
     }
 
     override func didReceiveMemoryWarning() {
@@ -21,5 +39,24 @@ class ViewController: UIViewController {
     }
 
 
+    @IBAction func indexChanged(sender: UISegmentedControl) {
+        switch segmentedControl.selectedSegmentIndex
+        {
+        case 0:
+            deckView.hidden = false
+            completedView.hidden = true
+            progressView.hidden = true
+        case 1:
+            deckView.hidden = true
+            completedView.hidden = true
+            progressView.hidden = false
+        case 2:
+            deckView.hidden = true
+            completedView.hidden = false
+            progressView.hidden = true
+        default:
+            break;
+        }
+    }
 }
 
